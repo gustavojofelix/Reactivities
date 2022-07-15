757addd3e675ea331fe30935e849d764aec84990
Gustavo Felix <gustavojofelix@hotmail.com>
2022-07-15 10:51:13 +0200

    Client side skeleton

--- API/Startup.cs
+++ API/Startup.cs
@@ -42,6 +42,13 @@ namespace API
             {
                 opt.UseSqlite(this.config.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -57,6 +64,8 @@ namespace API
             // app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
